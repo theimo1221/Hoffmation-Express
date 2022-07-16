2704440d0eacc9354705c6dd3d01662b5f7d0aa9
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2022-07-16 13:30:47 +0200

    Update Base and add some api methods

--- src/rest-service.ts
+++ src/rest-service.ts
@@ -3,6 +3,9 @@ import { Express } from 'express';
 import { API, iRestSettings, LogLevel, ServerLogService } from 'hoffmation-base';
 
 export class RestService {
+  public static get app(): Express {
+    return this._app;
+  }
   private static _app: Express;
 
   public static initialize(app: Express, config: iRestSettings): void {
@@ -40,6 +43,20 @@ export class RestService {
 
     this._app.get('/rooms/:roomId', (req, res) => {
       return res.send(API.getRoom(req.params.roomId));
+    });
+
+    this._app.get('/ac/power/:state', (req, res) => {
+      API.setAllAc(req.params.state === 'true');
+      res.status(200);
+      return res.send();
+    });
+
+    this._app.get('/ac/:acId/power/:state', (req, res) => {
+      return res.send(API.setAc(req.params.acId, req.params.state === 'true'));
+    });
+
+    this._app.get('/lamps/:deviceId/:state', (req, res) => {
+      return res.send(API.setLight(req.params.deviceId, req.params.state === 'true'));
     });
   }
 }
