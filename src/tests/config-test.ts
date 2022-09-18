1360362b727d40ed0acdc3dcff3a2b3f67509de0
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2022-09-18 10:13:41 +0200

    Update base

--- src/tests/config-test.ts
+++ src/tests/config-test.ts
@@ -1,7 +1,14 @@
-import { deviceConfig, Devices, HoffmationBase, HoffmationInitializationObject } from 'hoffmation-base/lib';
-import devJson from '../../config/private/devices.json';
-import config from '../../config/private/mainConfig.json';
-import { RoomImportEnforcer } from '../OwnRooms/RoomImportEnforcer';
+import {
+  deviceConfig,
+  Devices,
+  HeatingMode,
+  HoffmationBase,
+  HoffmationInitializationObject
+} from "hoffmation-base/lib";
+import devJson from "../../config/private/devices.json";
+import config from "../../config/private/mainConfig.json";
+import { RoomImportEnforcer } from "../OwnRooms/RoomImportEnforcer";
+import { SettingsService } from "hoffmation-base";
 
 export class ConfigTest {
   public static async start(): Promise<void> {
@@ -13,13 +20,18 @@ export class ConfigTest {
     await HoffmationBase.initializeBeforeIoBroker(init);
     new Devices(devJson as { [id: string]: deviceConfig }, new RoomImportEnforcer());
     console.log(`Devices amount: ${Object.keys(Devices.alLDevices).length}`);
+    if (SettingsService.heatMode === HeatingMode.Winter) {
+      console.log(`Desired Heating Mode Winter`);
+    } else if (SettingsService.heatMode === HeatingMode.Sommer) {
+      console.log(`Desired Heating Mode Summer`);
+    }
     process.exit(1);
   }
 }
 
 void ConfigTest.start();
 
-process.on('uncaughtException', (err) => {
+process.on("uncaughtException", (err) => {
   console.log(`Uncaught Exception: ${err.message}\n${err.stack}`);
   process.exit(1);
 });
