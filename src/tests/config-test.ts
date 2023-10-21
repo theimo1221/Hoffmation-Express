b35f25d7ea3e47e81994387d142b5457d23e3a7f
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2023-10-21 21:01:22 +0200

    Update Base

--- src/tests/config-test.ts
+++ src/tests/config-test.ts
@@ -8,7 +8,7 @@ import {
 import devJson from '../../config/private/devices.json';
 import config from '../../config/private/mainConfig.json';
 import { RoomImportEnforcer } from '../OwnRooms/RoomImportEnforcer';
-import { SettingsService } from 'hoffmation-base';
+import { API, SettingsService } from 'hoffmation-base';
 
 export class ConfigTest {
   public static async start(): Promise<void> {
@@ -18,7 +18,7 @@ export class ConfigTest {
     init.config.persistence = undefined;
     init.config.muell = undefined;
     await HoffmationBase.initializeBeforeIoBroker(init);
-    new Devices(devJson as { [id: string]: deviceConfig }, new RoomImportEnforcer());
+    new Devices(devJson as { [id: string]: deviceConfig }, new RoomImportEnforcer(), config);
     console.log(`Devices amount: ${Object.keys(Devices.alLDevices).length}`);
     if (SettingsService.heatMode === HeatingMode.Winter) {
       console.log(`Desired Heating Mode Winter`);
